fcb88d81246e12807b822ca0529a90b1fe653682
Vhd Valentin <valentin.haduong@gmail.com>
2024-12-19 16:32:06 +0100

    Battleship

---
+++ main.py
@@ -0,0 +1,9 @@
+from utility_functions import *
+from math import *
+from random import *
+
+
+input("Welcome to my humble abode. \n")
+input("Will you be able to face all the challenges you encounter?")
+
+challenges_menu()
